d85c04797e19fd774ca1647ca9719feaa2cc621b
Kael <kaelwd@gmail.com>
2017-10-11 03:11:45 +1100

    chore: Print an error if "git commit -m" is used
    This will force everyone to use this template for all commits

--- dev/prepare-commit-message.js
+++ dev/prepare-commit-message.js
@@ -11,4 +11,11 @@ if (commitType == null) {
   const newMessage = fs.readFileSync('.github/.git_commit_msg.txt')
   fs.writeFileSync(messageFile, newMessage)
   fs.appendFileSync(messageFile, currentMessage)
+} else if (commitType === 'message') {
+  const currentMessage = fs.readFileSync(messageFile, 'utf8')
+  if (currentMessage.startsWith('[release]')) return
+
+  console.error('"git commit -m" is not allowed in this project')
+  console.error('Please just use `git commit` and follow the template')
+  process.exitCode = 1
 }
